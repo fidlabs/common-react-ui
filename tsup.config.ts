fc32bfdcebce97ada3df125328d243f2989b9780
Maciej Trojan <maciej.trojan@neti-soft.com>
2025-01-29 12:05:51 +0100

    hotfix: fixed build errors and added build as a part of code check action [skip release]

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "tsup";
 
 const tsupConfig = defineConfig((options) => ({
-  entry: ["src/index.ts", "src/global.css", "src/**/*.ts", "src/**/*.tsx"],
+  entry: [
+    "src/index.ts",
+    "src/global.css",
+    "src/**/*.ts",
+    "src/**/*.tsx",
+    "!src/**/*.spec.ts",
+    "!src/**/*.spec.tsx",
+    "!src/**/*.stories.ts",
+    "!src/**/*.stories.tsx",
+  ],
   dts: true,
   clean: true,
   sourcemap: true,
