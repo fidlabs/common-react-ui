521bc2fb0f4b119562359d95e4e7c64161089e29
Maciej Trojan <maciej.trojan@neti-soft.com>
2025-01-29 11:37:46 +0100

    fix: fix eslint config and add build step to release job [skip release]

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginStorybook from "eslint-plugin-storybook";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"],
   },
-  { ignores: ["dist/"] },
+  { ignores: ["storybook-static", "!.storybook", "dist"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat["jsx-runtime"],
+  ...pluginStorybook.configs["flat/recommended"],
   {
     settings: {
       react: {
