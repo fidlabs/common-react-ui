552525ca56d4f987358f146a3409309a7ffdc0e8
Maciej Trojan <maciej.trojan@neti-soft.com>
2025-01-30 11:51:20 +0100

    feat: add Input component

---
+++ src/components/input.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "./button";
+import { Input } from "./input";
+
+const meta = {
+  title: "Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    disabled: {
+      control: "boolean",
+    },
+  },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    placeholder: "Email",
+  },
+};
+
+export const File: Story = {
+  args: {
+    type: "file",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    placeholder: "Email",
+    disabled: true,
+  },
+};
+
+export const WithButton: Story = {
+  render() {
+    return (
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input type="email" placeholder="Email" />
+        <Button type="submit">Subscribe</Button>
+      </div>
+    );
+  },
+};
