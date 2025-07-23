8f42e48207b7697f172ef06dc3ffba07261f9940
Maciej Trojan <maciej.trojan@neti-soft.com>
2025-07-23 16:03:25 +0200

    fix: use named export in an attempt to fix imports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
-export * from "@/components/button";
-export * from "@/components/card";
-export * from "@/components/form";
-export * from "@/components/input";
-export * from "@/components/label";
-export * from "@/components/skeleton";
-export * from "@/components/alert";
-export * from "@/components/select";
-export * from "@/components/scroll-area";
+export { Button } from "@/components/button";
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/card";
+export {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+} from "@/components/form";
+export { Input } from "@/components/input";
+export { Label } from "@/components/label";
+export { Skeleton } from "@/components/skeleton";
+export { Alert, AlertDescription, AlertTitle } from "@/components/alert";
+export type { AlertProps } from "@/components/alert";
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/select";
+export { ScrollArea, ScrollBar } from "@/components/scroll-area";
 export { cn } from "@/lib/utils";
