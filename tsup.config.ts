54d2118ad68a8deaa0baa17c13a8b8b2d6c7da1a
Maciej Trojan <maciej.trojan@neti-soft.com>
2025-07-24 11:56:03 +0200

    BREAKING CHANGE: make lucide-react peer dependency and don't export unnecessary files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,22 +1,13 @@
 import { defineConfig } from "tsup";
 
 const tsupConfig = defineConfig((options) => ({
-  entry: [
-    "src/index.ts",
-    "src/global.css",
-    "src/**/*.ts",
-    "src/**/*.tsx",
-    "!src/**/*.spec.ts",
-    "!src/**/*.spec.tsx",
-    "!src/**/*.stories.ts",
-    "!src/**/*.stories.tsx",
-  ],
+  entry: ["src/index.ts", "src/global.css"],
   dts: true,
   clean: true,
   sourcemap: true,
   minify: !options.watch,
   treeshake: true,
-  external: ["react", "react/jsx-runtime"],
+  external: ["react", "react/jsx-runtime", "lucide-react"],
 }));
 
 export default tsupConfig;
